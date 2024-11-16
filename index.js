2d525b25701cef01b9969325d8484c79324bc294
Parvatham Siva Mallikarjun <112698902+PSivaMallikarjun@users.noreply.github.com>
2024-11-16 05:40:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Thank you for registering for the Hot Balloon Ride event!");
+});
